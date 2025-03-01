53922ca71dbc9db7a3622169f8553b84c78282b3
Mrcoolguy18 <lildezul8@gmail.com>
2025-03-01 12:35:17 -0500

    Update EaglerHomes.js

--- CommunityMods/eaglerforgeX/EaglerHomes.js
+++ CommunityMods/eaglerforgeX/EaglerHomes.js
@@ -1,6 +1,6 @@
 ModAPI.require("player");
 ModAPI.addEventListener("sendchatmessage", function (event) {
-    if(event.message === "{sethome}"){
+    if(event.message === "/sethome"){
         localStorage.setItem("HomePosX", Math.round(ModAPI.player.x));
         localStorage.setItem("HomePosY", Math.round(ModAPI.player.y));
         localStorage.setItem("HomePosZ", Math.round(ModAPI.player.z));
